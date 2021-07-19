0fbf6d0e4ad45d56277ca785cec88eeaa591b640
Antonio <antonioru1997@gmail.com>
2021-07-19 15:00:41 +0200

    FINE BONUS

--- BONUS/js/main.js
+++ BONUS/js/main.js
@@ -3,7 +3,10 @@
  - Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 
  // Creo una variabile per indicare il numero di "interazioni"
- var interactionN = 100; 
+ var interactionN = 100;
+
+ // Creo una variabile per "stampare" la lista nell'HTML
+ var stamp = "";
  
  // Uso il for per generare i numeri da 1 a 100
  for (var i=0; i < interactionN; i++)
@@ -12,17 +15,24 @@
      if ((number % 3 == 0) && (number % 5 == 0))
      {
         console.log("FizzBuzz");
+        stamp += "<li class='fizz-buzz'>FizzBuzz</li>";
      }
      else if (number % 3 == 0)
      {
-         console.log("Fizz");
+        console.log("Fizz");
+        stamp += "<li class='fizz'>Fizz</li>";
      }
      else if (number % 5 == 0)
      {
          console.log("Buzz");
+         stamp += "<li class='buzz'>Buzz</li>";
      }
      else
      {
          console.log (number);
+         stamp += "<li>" + number + "</li>";
      }
  }
+
+ // Stampo nell'html la lista di numeri.
+ document.getElementById("list").innerHTML =stamp;
